a13cc8536116374ed73744cb25cb864af8ee9efe
kamalakkannan6002-byte <kamalakkannan6002@gmail.com>
2026-07-14 14:00:37 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.py
@@ -0,0 +1,21 @@
+# Last updated: 7/14/2026, 2:00:36 PM
+class Solution:
+    def minDepth(self, root):
+        if root is None:
+            return 0
+
+        if root.left is None and root.right is None:
+            return 1
+
+        if root.left is None:
+            return 1 + self.minDepth(root.right)
+
+        if root.right is None:
+            return 1 + self.minDepth(root.left)
+
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+
+
+
+
