0e8f3e3a4686f9acfb261c4f81f196a03d97f66a
kamalakkannan6002-byte <kamalakkannan6002@gmail.com>
2026-07-14 14:01:18 +0530

    Create 18-4sum

---
+++ 18-4sum/18-4sum.py
@@ -0,0 +1,55 @@
+# Last updated: 7/14/2026, 2:01:17 PM
+class Solution:
+    def fourSum(self, nums, target):
+
+        nums.sort()
+        result = []
+        n = len(nums)
+
+        for i in range(n - 3):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        result.append([
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ])
+
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return result
+
+
+nums = [1,0,-1,0,-2,2]
+target = 0
+
+obj = Solution()
+print(obj.fourSum(nums, target))
