c03530d961851c9e504732cf2fd24addbccff5f8
kamalakkannan6002-byte <kamalakkannan6002@gmail.com>
2026-07-14 14:00:20 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 2:00:19 PM
+class Solution:
+    def isHappy(self, n):
+        seen = set()
+
+        while n != 1:
+            if n in seen:
+                return False
+
+            seen.add(n)
+
+            total = 0
+            for digit in str(n):
+                total += int(digit) ** 2
+
+            n = total
+
+        return True
