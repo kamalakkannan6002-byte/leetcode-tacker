49c0e38972a80f7f046f5c7d7acb450793eccc06
kamalakkannan6002-byte <kamalakkannan6002@gmail.com>
2026-07-14 14:00:39 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,42 @@
+# Last updated: 7/14/2026, 2:00:38 PM
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+
+class solution:
+    def range(self, nums1, m, num2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while i >=0 and j >=0:
+            if nums[i] > num[j]:
+                nums[k] = nums[i]
+                i -= 1
+            else:
+                nums1[k] = nums[j]
+                j -= 1
+                
+                while j >= 0:
+                    nums1[k] = nums2[j]
+                    j -= 1
+                    k -= 1
+                                  
