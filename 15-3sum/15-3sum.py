cf6ee935afcda97b0e28438f6426c4e789777a5b
kamalakkannan6002-byte <kamalakkannan6002@gmail.com>
2026-07-14 14:01:25 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,44 @@
+# Last updated: 7/14/2026, 2:01:24 PM
+class Solution:
+    def threeSum(self, nums):
+
+        nums.sort()
+        result = []
+
+        for i in range(len(nums)):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
+
+
+
+nums = [-1,0,1,2,-1,-4]
+
+obj = Solution()
+print(obj.threeSum(nums))
