082dcf7e5f9a63738f0cdb75e440cb94d9527803
kamalakkannan6002-byte <kamalakkannan6002@gmail.com>
2026-07-14 14:01:29 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.py
@@ -0,0 +1,32 @@
+# Last updated: 7/14/2026, 2:01:28 PM
+class Solution:
+    def intToRoman(self, num: int) -> str:
+
+        values = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+
+        symbols = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
+
+        result = ""
+
+        for i in range(len(values)):
+
+            while num >= values[i]:
+                result += symbols[i]
+                num -= values[i]
+
+        return result
+
+
+num = 1994
+
+obj = Solution()
+print(obj.intToRoman(num))
+        
